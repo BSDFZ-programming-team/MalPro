c191807eed98748cc02179d7f07523bc4aaa08a4
D0WE1L1N <1372449351@qq.com>
2024-07-27 23:39:43 +0800

    v0.5.4 readme update
    license changed

--- server.py
+++ server.py
@@ -310,4 +310,4 @@ async def upload(file: UploadFile = File(...)):
         '''
     return html
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=7777)
